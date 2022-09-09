492762dee6f015004b8c8d9ad2543aacf60f753b
Helio Giuseppe Pizarro Puentes <helio.pizarro@softtekprojects.com>
2022-09-09 17:59:45 -0500

    diseño login y splash

---
+++ app/src/main/java/com/venta/ventapps/Login/Login.java
@@ -0,0 +1,16 @@
+package com.venta.ventapps.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.venta.ventapps.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/venta/ventapps/Splash.java
@@ -0,0 +1,44 @@
+package com.venta.ventapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.venta.ventapps.Login.Login;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        Animation animation1= AnimationUtils.loadAnimation(this,R.anim.desplazar_arriba);
+        Animation animation2= AnimationUtils.loadAnimation(this,R.anim.desplazar_abajo);
+
+        TextView marca=findViewById(R.id.nomapp);
+        TextView by=findViewById(R.id.bytext);
+        ImageView logo=findViewById(R.id.logosp);
+
+        marca.setAnimation(animation2);
+        by.setAnimation(animation2);
+        logo.setAnimation(animation1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
