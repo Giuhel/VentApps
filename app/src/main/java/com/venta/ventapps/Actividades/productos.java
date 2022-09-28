f7cfc6e35ae2f58b7b96001d446b9c58f1cea7b5
Giuhel <giuseppe2508@hotmail.com>
2022-09-28 17:57:40 -0500

    diseño actividad productos v1

---
+++ app/src/main/java/com/venta/ventapps/Actividades/productos.java
@@ -0,0 +1,14 @@
+package com.venta.ventapps.Actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.venta.ventapps.R;
+
+public class productos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productos);
+    }
+}
