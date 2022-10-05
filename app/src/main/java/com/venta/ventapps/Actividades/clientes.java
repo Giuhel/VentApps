7393b5b88adfcbc7b25d075e67b3b39f5818b35f
Giuhel <giuseppe2508@hotmail.com>
2022-10-05 18:06:19 -0500

    Diseño actividad cliente v1

---
+++ app/src/main/java/com/venta/ventapps/Actividades/clientes.java
@@ -0,0 +1,18 @@
+package com.venta.ventapps.Actividades;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.venta.ventapps.R;
+
+public class clientes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clientes);
+    }
+
+    public void onClick(View view) {
+    }
+}
