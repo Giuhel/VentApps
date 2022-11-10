f5444b1c0be6e893a8def8f229ee13f10f83687b
Giuhel <giuseppe2508@hotmail.com>
2022-11-10 17:54:08 -0500

    acercade v1

--- app/src/main/java/com/venta/ventapps/fragmentos/acerca_de.java
+++ app/src/main/java/com/venta/ventapps/fragmentos/acerca_de.java
@@ -1,10 +1,15 @@
 package com.venta.ventapps.fragmentos;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import com.venta.ventapps.R;
 
 /**
@@ -23,16 +28,7 @@ public class acerca_de extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment acerca_de.
-     */
-    // TODO: Rename and change types and number of parameters
+    
     public static acerca_de newInstance() {
 
         Bundle args = new Bundle();
@@ -55,10 +51,71 @@ public class acerca_de extends Fragment {
         }
     }
 
+    LinearLayout instragram,facebook,tiktok;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_acerca_de, container, false);
+        View vista =inflater.inflate(R.layout.fragment_acerca_de, container, false);
+
+        instragram=vista.findViewById(R.id.btnIG);
+        facebook=vista.findViewById(R.id.btnFB);
+        tiktok=vista.findViewById(R.id.btntiktok);
+
+        eventoBotones();
+
+        return vista;
+    }
+
+    private void eventoBotones() {
+
+        Intent intent=null;
+
+        instragram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                llamarWEB(intent,"ig");
+            }
+        });
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                llamarWEB(intent,"fb");
+            }
+        });
+        tiktok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                llamarWEB(intent,"tiktok");
+            }
+        });
+    }
+    private void llamarWEB(Intent intent,String web){
+        String url = null;
+        if(web=="ig"){
+            url="https://www.instagram.com/helio_pizarro/";
+        }else if(web=="fb"){
+            url="https://www.facebook.com/helio.pizarro/";
+        }else if(web=="tiktok"){
+            url="https://www.tiktok.com/@giuhel_pip?lang=es";
+        }
+        intent= new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+    }
+
+    public void onClick(View view) {
+        //Intent intent=null;
+        switch (view.getId()){
+            case R.id.btnIG:
+                //intent= new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/helio_pizarro/"));
+                Toast.makeText(getContext(),"instagram",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        //startActivity(intent);
+    }
+
+
+    public void enlace(View view) {
     }
 }
